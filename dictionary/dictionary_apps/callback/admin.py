ec0ef1f56bfc1482b284209a8721ac690979bf89
Alexvargame <direct.fito@gmail.com>
2025-08-20 09:18:50 +0300

    Message_telegram

--- dictionary/dictionary_apps/callback/admin.py
+++ dictionary/dictionary_apps/callback/admin.py
@@ -1,9 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from .models import SiteMessage
-# @admin.register(SiteMessage)
-# class SiteMessageAdmin(admin.ModelAdmin):
-#
-#     list_display = ('id', 'user', 'text', 'is_answered', 'answer_text', 'created_at', 'answered_at')
-#
-#
+from .models import SiteMessage
+@admin.register(SiteMessage)
+class SiteMessageAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'user', 'text', 'is_answered', 'answer_text', 'created_at', 'answered_at')
+
+
