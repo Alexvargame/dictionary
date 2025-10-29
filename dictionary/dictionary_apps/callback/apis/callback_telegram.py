160e1bb2dd68320b3ea734c161b03166469bf150
Alexvargame <direct.fito@gmail.com>
2025-10-29 09:11:34 +0300

    BOT_2

--- dictionary/dictionary_apps/callback/apis/callback_telegram.py
+++ dictionary/dictionary_apps/callback/apis/callback_telegram.py
@@ -32,6 +32,7 @@ def handle_command_user_message(chat_id, text):
     text_clean = text.replace('#012', ' ').replace('\n', ' ').replace('\r', ' ').replace("\xa0", " ").strip()
     print('CLELREAN', text_clean)
     parts = text_clean.split(" ", 2)  # ['/message_user', '@alex', 'Привет, тест!']
+    print(parts)
     if len(parts) < 3:
         send_message(chat_id, "❌ Использование: /message_user <username|chat_id> <текст>")
         return
