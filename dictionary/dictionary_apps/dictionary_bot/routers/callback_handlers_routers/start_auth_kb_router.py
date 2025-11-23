1d6b01e77bee1205f11763cd7b799f7b94be2a36
Alexvargame <direct.fito@gmail.com>
2025-11-23 10:59:05 +0300

    webhook_new

--- dictionary/dictionary_apps/dictionary_bot/routers/callback_handlers_routers/start_auth_kb_router.py
+++ dictionary/dictionary_apps/dictionary_bot/routers/callback_handlers_routers/start_auth_kb_router.py
@@ -5,12 +5,6 @@ from dictionary.dictionary_apps.dictionary_bot.keyboards.kb_auth_reg import RegD
 
 router = Router(name='reg_auth')
 
-# Test callback
-# @router.callback_query()
-# async def debug_all_callbacks(callback_query: CallbackQuery):
-#     print("RAW CALLBACK:", callback_query.data)
-#     await callback_query.answer()
-
 @router.callback_query(
     RegData.filter(F.action == RegDataAction.reg),
                        )
@@ -24,7 +18,7 @@ async def start_reg(callback_query: CallbackQuery):
 @router.callback_query(RegData.filter(F.action == RegDataAction.auth))
 async def start_auth(callback_query: CallbackQuery):
 
-    print("AUTH", callback_query.answer(text='auth'))
+    print("AUTH")
     await callback_query.answer(
         text="Введите пароль:",
         reply_markup=types.ReplyKeyboardRemove()
